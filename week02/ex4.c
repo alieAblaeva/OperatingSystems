79a5ec42582833e9f7086b37cf138a2673ace5ad
alieAblaeva <118367463+alieAblaeva@users.noreply.github.com>
2023-09-19 20:51:47 +0300

    Add files via upload

---
+++ week02/ex4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void count(char string[], char letter){
+	int counter = 0;
+	for (int i = 0; i<strlen(string); i++){
+		if (string[i] == letter)
+			counter++;
+	}
+	printf("%c:%d", letter, counter);
+}
+
+void countAll(char string[]){
+	for(int i = 0; i<strlen(string); i++){
+		count(string, string[i]);
+		if(i!= strlen(string)-1)
+			printf(", ");
+	}
+}
+
+int main(){
+	char string[256];
+	scanf("%s", string);
+	countAll(string);
+	return 0;
+}
